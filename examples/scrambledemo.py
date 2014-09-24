a785379f310f99e2023f38178d02bb0d7c04739d
mno <mno00.email@gmail.com>
2014-09-23 18:34:52 -0600

    first commit

--- examples/scrambledemo.py
+++ examples/scrambledemo.py
@@ -5,7 +5,7 @@ glitchAmt = 10
 text = "This is a demonstration text string that shall be randomly glitched and destroyed."
 
 for x in range(22):
-    scrambledTexts.append( scramble.scramble_text(glitchAmt, text) )
+    scrambledTexts.append( scramble_text(glitchAmt, text) )
     glitchAmt += 10
 
 print ""
